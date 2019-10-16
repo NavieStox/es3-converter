37e057523339a0902e2f760de7eb66d9976df9cf
naviestox <task4610@gmail.com>
2019-10-17 01:38:42 +0900

    auto bundle with nodemon

--- bundle.js
+++ bundle.js
@@ -6,13 +6,11 @@ const glob = require("glob");
 const srcDir = "./src";
 const distDir = "./bin";
 
-// ./src以下のjsファイルのリストを取得する。ただし_から始まるファイルとmodulesディレクトリは除外する。
 const entries = glob.sync("**/*.js", {
   ignore: ["modules/**/*.js", "**/_*.js"],
   cwd: srcDir
 });
 
-//リストアップしたjsファイルをバンドルする。
 for (let entry of entries) {
   const inputOptions = {
     input: path.resolve(srcDir, entry),
